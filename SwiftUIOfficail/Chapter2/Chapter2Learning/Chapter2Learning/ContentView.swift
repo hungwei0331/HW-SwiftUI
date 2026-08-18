42abb5a4a7e25dd13a0a3a4d3c15334c2750fcc8
hungwei0331 <hungwei0331@gmail.com>
2026-08-18 10:37:54 +0800

    update with chapter2

--- SwiftUIOfficail/Chapter2/Chapter2Learning/Chapter2Learning/ContentView.swift
+++ SwiftUIOfficail/Chapter2/Chapter2Learning/Chapter2Learning/ContentView.swift
@@ -7,17 +7,48 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
+        HStack {
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+            
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
+        }
+    }
+}
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        isRainy == true ? "cloud.rain.fill" : "sun.max.fill"
+    }
+    
+    var iconColor: Color {
+        isRainy == true ? Color.blue : Color.yellow
+    }
+    
+    var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(day).font(.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("High: \(high)")
+                .fontWeight(.semibold)
+            Text("Low: \(low)")
+                .fontWeight(.medium)
+                .foregroundStyle(Color.secondary)
         }
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
