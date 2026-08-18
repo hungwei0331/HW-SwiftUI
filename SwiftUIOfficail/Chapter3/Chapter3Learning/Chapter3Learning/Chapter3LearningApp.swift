93e3f294d7524430d84b2667c5163bd2ff02c19e
hungwei0331 <hungwei0331@gmail.com>
2026-08-18 14:13:51 +0800

    init chpater3

---
+++ SwiftUIOfficail/Chapter3/Chapter3Learning/Chapter3Learning/Chapter3LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter3LearningApp.swift
+//  Chapter3Learning
+//
+//  Created by 陳泓維 on 2026/8/18.
+//
+
+import SwiftUI
+
+@main
+struct Chapter3LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIOfficail/Chapter3/Chapter3Learning/Chapter3LearningTests/Chapter3LearningTests.swift
@@ -0,0 +1,19 @@
+//
+//  Chapter3LearningTests.swift
+//  Chapter3LearningTests
+//
+//  Created by 陳泓維 on 2026/8/18.
+//
+
+import Testing
+@testable import Chapter3Learning
+
+struct Chapter3LearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
